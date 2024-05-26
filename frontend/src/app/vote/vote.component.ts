b3df21d0301a67cd08aeea5f3488521cdb247407
Rickard Nyman <4slovb@gmail.com>
2024-05-26 14:07:05 +0200

    cards with more discrete arrows

--- frontend/src/app/vote/vote.component.ts
+++ frontend/src/app/vote/vote.component.ts
@@ -8,32 +8,32 @@ interface Item {
 
 const green_list = [
   {
-    title: 'Code',
-    subtitle: 'This project',
+    title: 'Pizza',
+    subtitle: 'Good pizza place',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   },
   {
-    title: 'Make lunch',
+    title: 'Taco',
   },
   {
-    title: 'Play game',
-    subtitle: 'Some game',
+    title: 'Burger',
+    subtitle: 'YumBun',
     text: 'Et harum quidem rerum facilis est et expedita distinctio.',
   },
   {
-    title: 'Watch anime',
+    title: 'Thai',
     text: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?',
   },
 ];
 
 const red_list = [
   {
-    title: 'Shop',
+    title: 'Sushi',
     text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    title: 'Clean',
-    subtitle: 'Mop the floors',
+    title: 'Pizza',
+    subtitle: 'Dumpstertruck pizzas',
     text: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.',
   },
 ];
