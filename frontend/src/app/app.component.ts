e36cae4e64202e47703fc24cbdf70804fc41f6d1
Rickard Nyman <4slovb@gmail.com>
2024-05-26 09:40:10 +0200

    nav setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ResultComponent } from './result/result.component';
 import { VoteComponent } from './vote/vote.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, ResultComponent, VoteComponent],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    ResultComponent,
+    VoteComponent,
+    PageNotFoundComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ResultComponent } from './result/result.component';
+import { VoteComponent } from './vote/vote.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'vote', component: VoteComponent },
+  { path: 'result', component: ResultComponent },
+  { path: '', redirectTo: '/vote', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
