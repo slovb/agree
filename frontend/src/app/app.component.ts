28f942943b96223d4aa6f5374639213b8205460c
Rickard Nyman <4slovb@gmail.com>
2024-05-26 09:12:39 +0200

    nav, vote, result generated

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { ResultComponent } from './result/result.component';
+import { VoteComponent } from './vote/vote.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, ResultComponent, VoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
