e9dbeac0d7b7e0637eb4f68f089f8a275e076cac
Rickard Nyman <4slovb@gmail.com>
2024-06-09 14:37:40 +0200

    send mock poll

--- backend/agree/poll/poll.py
+++ backend/agree/poll/poll.py
@@ -1,8 +1,59 @@
+from enum import Enum
+
 from flask import Blueprint, jsonify
 
 bp = Blueprint("poll", __name__, url_prefix="/poll")
 
 
+class YayNay(str, Enum):
+    YAY = "yay"
+    NAY = "nay"
+
+
+ideaList = [
+    {
+        "id": "asdfasdf",
+        "title": "Pizza",
+        "yaynay": YayNay.YAY,
+        "subtitle": "Round joy",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "motions": ["Good pizza place"],
+    },
+    {
+        "id": "a0sdfasdf",
+        "title": "Taco",
+        "yaynay": YayNay.YAY,
+    },
+    {
+        "id": "as1dfasdf",
+        "title": "Burger",
+        "yaynay": YayNay.YAY,
+        "subtitle": "YumBun",
+        "text": "Et harum quidem rerum facilis est et expedita distinctio.",
+    },
+    {
+        "id": "asd2fasdf",
+        "title": "Thai",
+        "yaynay": YayNay.YAY,
+        "text": "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
+    },
+    {
+        "id": "asdf3asdf",
+        "title": "Sushi",
+        "yaynay": YayNay.NAY,
+        "text": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "id": "asdfa4sdf",
+        "title": "Pizza",
+        "yaynay": YayNay.NAY,
+        "subtitle": "Round joy",
+        "text": "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
+        "motions": ["Dumpstertruck pizzas"],
+    },
+]
+
+
 @bp.route("/", methods=["GET"])
 def get():
-    return jsonify("heya!")
+    return jsonify(ideaList)
--- frontend/src/app/poll/poll.service.ts
+++ frontend/src/app/poll/poll.service.ts
@@ -5,49 +5,6 @@ import { Idea } from '../../idea/idea';
 import { YayNay } from '../yay-nay/yay-nay';
 import { PollModule } from './poll.module';
 
-const item_list = [
-  {
-    id: 'asdfasdf',
-    title: 'Pizza',
-    yaynay: YayNay.yay,
-    subtitle: 'Round joy',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    motions: ['Good pizza place'],
-  },
-  {
-    id: 'a0sdfasdf',
-    title: 'Taco',
-    yaynay: YayNay.yay,
-  },
-  {
-    id: 'as1dfasdf',
-    title: 'Burger',
-    yaynay: YayNay.yay,
-    subtitle: 'YumBun',
-    text: 'Et harum quidem rerum facilis est et expedita distinctio.',
-  },
-  {
-    id: 'asd2fasdf',
-    title: 'Thai',
-    yaynay: YayNay.yay,
-    text: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?',
-  },
-  {
-    id: 'asdf3asdf',
-    title: 'Sushi',
-    yaynay: YayNay.nay,
-    text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  },
-  {
-    id: 'asdfa4sdf',
-    title: 'Pizza',
-    yaynay: YayNay.nay,
-    subtitle: 'Round joy',
-    text: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.',
-    motions: ['Dumpstertruck pizzas'],
-  },
-];
-
 const POLL_URL = 'http://localhost:5000/poll/';
 const VOTE_URL = 'http://localhost:5000/vote/';
 const headers = new HttpHeaders().set('Content-type', 'application/json');
@@ -60,9 +17,13 @@ export class PollService {
 
   public async get(id: string): Promise<Idea[]> {
     let res = await firstValueFrom(
-      this.http.get(POLL_URL, { responseType: 'json', headers: headers })
+      this.http.get<Idea[]>(POLL_URL, {
+        responseType: 'json',
+        headers: headers,
+      })
     );
-    return item_list;
+    console.log(res);
+    return res;
   }
 
   public vote(ideas: Idea[]): void {
