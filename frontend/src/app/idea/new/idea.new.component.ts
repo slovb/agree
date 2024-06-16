2e5f6f18d1eeddfe84f1055e7197ea502214a6c7
Rickard Nyman <4slovb@gmail.com>
2024-06-16 15:59:36 +0200

    adding and removing motions roughly

--- frontend/src/app/idea/new/idea.new.component.ts
+++ frontend/src/app/idea/new/idea.new.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'idea-new',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './idea.new.component.html',
   styleUrl: './idea.new.component.scss',
 })
-export class IdeaNewComponent {}
+export class IdeaNewComponent {
+  motions = this._formBuilder.array([this._formBuilder.control('')]);
+  newForm = this._formBuilder.group({
+    title: ['', Validators.required],
+    subtitle: [''],
+    motions: this.motions,
+    text: [''],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  addMotion(): void {
+    this.motions.push(this._formBuilder.control(''));
+  }
+
+  removeMotion(index: number): void {
+    this.motions.removeAt(index);
+  }
+}
